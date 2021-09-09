3ad1d2e57849a65c0914477c40dc4e42db6ade6a
Anna Bichevskaya <anna.bichevskaja@gmail.com>
2021-09-09 20:11:00 +0300

    add page objects

---
+++ test/pageobjects/menu.page.js
@@ -0,0 +1,136 @@
+import Page from "./page";
+
+class Menu extends Page{
+
+    async open (){
+        super.open('/')
+    }
+
+    get regulationsLink() { return $('') }; //нормативные документы
+
+    get documetnsUpperComunitiesLink() { return $('') }; //документы вышестоящих организаций
+
+    get systemOfMenegmentQualityLink(){ return $('') };//система менеджмента качества   
+
+    get collegeOrdersLink(){ return $('') }; // приказы по колледжу
+
+    get collegePositionsLink() { return $('') }; // положения
+
+    // о колледже 
+    get collegeHistoryLink() { return $('') } // история
+
+    get collegeStructureLink() { return $('') } // структура колледжа 
+
+    get metodistsLink() { return $('') }; // методисты 
+
+    get educationWorksWithStudentsLink() { return $('') };
+
+    get personnelServiceLink() { return $('') } //кадровая служба 
+
+    get bookkeepingLink() { return $('') } //Бухгалтерия
+
+    get secretaryLink() { return $('') } //Секретарь
+
+    get libraryLink() {return $('')} // Библиотека 
+
+    get informationGrupLink() { return $('')} // Группа по информации
+
+    get educationWorkshopLink() { return $('')} //учебно-производственные мастерские
+
+    get belayaRusLink() { return $('')} // РOO Белая Русь
+
+    get tradeUnionOrganisationLink() { return $('') } // Профсозная организация
+
+    get graduatesLink() {return $('') } // выпускники колледжа
+
+    get bankChildProdigyLink() { return $('')} // одаренные дети
+
+    get galeryLink() { return $('')} // галерея
+
+    get virtualExcursionLink() { return $('')} //Виртуальная экскурсия
+
+    get resoursesCenterLink() {return $('')} //ресурсный центр
+
+    //Образование 
+
+    get departmentOfComputerTechnologyLink() {return $('')}
+
+    get radioEngineeringDepartmentLink() { return $('')}
+
+    get departmentOfElectronicsLink() {return $('')}
+
+    get POITLink() {return $('')}
+
+    get informationTechnologiesLink() {return $('')}
+
+    get PIPRSLink() { return $('')}
+
+    get EVSLink() { return $('')}
+
+    get MINTISLink() { return $('')}
+
+    get TERESLink() { return $('')}
+
+    get PMSLink() {return $('')}
+
+    get generalTechnicalDisciplinesLink() { return $('')}
+
+    get MathAndSienceLink()  {return $('')}
+
+    get socialDisciplinesLink() { return $('')}
+
+    get PEDisciplinesLink() { return $('')}
+
+    get schoolInCollegeLink() { return $('')}
+
+    get otherEducationLink() { return $('')}
+
+    get conferenceLink() { return $('')}
+
+    get experimentalActivityLink() {return $('')}
+
+    // Воспитание 
+
+    get structureOfEducationWorkLink() {return $('')} //структура работы с молодежью
+
+    get selfControlCenterLink() {return $('')} //совет самоуправления
+
+    get BRSMLink() { return $('')} //БРСМ
+
+    get profComLink() { return $('')} // профком
+
+    get specialistsSPPSLink() { return $('')} // специалисты СППС
+
+    get mainTasksAndDirectionsLink() { return $('')} //основные задачи и направления 
+
+    get personalSuccessWorkshopLink() {return $('')} //мастерская личного успеха
+
+    get informationForStudentsLink() { return $('')} // информация для студентов
+
+    get informationForTeachersLink()  {return $('')} // информация для преподавателей
+
+    get infoCenterLink() { return $('')} // инфоцентр
+
+    get activPersonSchoolLink() { return $('')} // школа активного гражданина
+
+    get lawAndLegalOrderLink() { return $('')} // Законность и правопорядок
+
+    get yearOfLittleHomelandLink() { return $('')} // год малой родины
+
+    get unionDayOfInformationLink() { return $('')} // Единый день информации
+
+    get interestingToKnowLink() {return $('')} // интересно знать
+
+    // учащимся 
+
+    
+
+
+
+
+
+
+
+
+
+}
